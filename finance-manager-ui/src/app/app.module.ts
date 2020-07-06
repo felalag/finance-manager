b4b27c39f245032ec75e2cb2097e46d7d3282b01
Ira Boboyed <iraboboyed@gmail.com>
2020-07-06 23:19:59 +0300

    * switched to angular version 10 * added login page markup * added navigation bar markup

--- finance-manager-ui/src/app/app.module.ts
+++ finance-manager-ui/src/app/app.module.ts
@@ -3,14 +3,32 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {LoginComponent} from './default-layout/login/login.component';
+import {NavigationBarComponent} from './default-layout/navigation-bar/navigation-bar.component';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
